fd7e464086aff2f7eee5c4cbf4aec31bef8d5a19
Evan Lloyd <evan.t.lloyd@gmail.com>
2015-03-20 00:47:01 -0700

    new tests

--- tn_tests.hs
+++ tn_tests.hs
@@ -7,6 +7,8 @@ main = do assert (tn (TN 1 [1] [[5]] (Set.fromList []))) $ return []
           putStrLn $ show $ stochasticity ptn1 stoch1
           putStrLn $ show $ stochasticity ptn2 stoch2
           putStrLn $ show $ stochasticity ptn3 stoch3
+          putStrLn $ show $ stochasticity ptn4 stoch4
+          putStrLn $ show $ stochasticity ptn5 stoch5
           putStrLn "All tests succeeded"
        where
           ptn1 = (TN 4 [1,2,3,2] [[4], [4,3], [4,4,4], [3,3]] (closeEdges [(1,1,3,1), (2,1,3,2), (2,2,4,1)]))
@@ -15,3 +17,7 @@ main = do assert (tn (TN 1 [1] [[5]] (Set.fromList []))) $ return []
           stoch2 = stochList [ [[]], [[1]] ]
           ptn3 = (TN 4 [2,2,3,3] [[4,3], [4,3], [4,4,4], [3,3,3]] (closeEdges [(1,1,3,1), (1,2,4,2), (2,1,3,2), (2,2,4,1)]))
           stoch3 = stochList [ [[2]], [[1]], [[1],[2],[3]], [[1],[2],[3]] ]
+          ptn4 = (TN 4 [2,3,2,1] [[3,3],[4,4,4],[4,3],[4]] (closeEdges [(4,1,2,1), (3,1,2,2), (3,2,1,1)]))
+          stoch4 = stochList [ [[1],[2]], [[1],[2],[3]], [[1]], [[]] ]
+          ptn5 = (TN 2 [3,3] [[4,4,3], [4,4,4]] (closeEdges [(1,1,2,1), (1,2,2,2)]))
+          stoch5 = stochList [ [[2]], [[1],[2],[3]] ]
